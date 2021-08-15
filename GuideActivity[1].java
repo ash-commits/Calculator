93f96df170dfdfb68415c9b146432a420b617c87
ash-commits <85698458+ash-commits@users.noreply.github.com>
2021-08-15 20:53:00 +0530

    Add files via upload

---
+++ GuideActivity[1].java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GuideActivity extends AppCompatActivity {
+private ImageButton close;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        close=findViewById(R.id.closeHow);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+}
